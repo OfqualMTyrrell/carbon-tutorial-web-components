6ce93d457f5655eb401a3ff1476e82d7f7458050
Michael Tyrrell <108464798+OfqualMTyrrell@users.noreply.github.com>
2025-11-03 15:15:01 +0000

    Addign web components example page and links to it

---
+++ demo/web-components-example.js
@@ -0,0 +1,21 @@
+/**
+ * Page A: Filter Panel + Data Table Layout
+ *
+ * Constitution compliance:
+ * - Styles imported FIRST (before components)
+ * - Page-specific JS module for code-splitting
+ * - Components imported on-demand
+ */
+
+// 1. Import styles FIRST (constitution requirement: Import Discipline)
+import './demo-style.scss';
+
+// 2. Import Carbon components (only what this page needs)
+import '@carbon/web-components/es/components/skip-to-content/index';
+import '@carbon/web-components/es/components/data-table/index';
+import '@carbon/web-components/es/components/ui-shell/index';
+import '@carbon/web-components/es/components/checkbox/index';
+import '@carbon/web-components/es/components/dropdown/index';
+import '@carbon/web-components/es/components/text-input/index';
+// Grid (layout) component
+import '@carbon/web-components/es/components/grid/index';
